3f67f03a8f92f4f066810160f6d4cccefd5cafa4
Barry <barrymay@Barrys-MacBook-Pro.local>
2019-07-13 08:01:58 -0400

    Flipping phases

--- components/game/PhaseButton.tsx
+++ components/game/PhaseButton.tsx
@@ -1,14 +1,22 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import { CSSProperties } from '@emotion/serialize';
 import { Merge } from '@react-spring/shared';
-import { ButtonHTMLAttributes, useEffect } from 'react';
+import { ButtonHTMLAttributes, MouseEvent, useEffect, useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 
 type PhaseState = 'default' | 'complete' | 'new-complete';
-interface ISpringType {
+interface ISpringType extends CSSProperties {
   color: string;
   backgroundColor: string;
 }
+
+const getFlipState = (
+  isCompleted: boolean,
+  currentFlip: boolean,
+): PhaseState => {
+  return isCompleted ? 'complete' : currentFlip ? 'new-complete' : 'default';
+};
 
 const getStateColors = (value: PhaseState) => {
   switch (value) {
@@ -19,25 +27,52 @@ const getStateColors = (value: PhaseState) => {
       };
     case 'new-complete':
       return {
-        color: 'white',
-        backgroundColor: 'blue',
+        transformStyle: 'preserve-3d',
+        transform: 'rotateY(180deg)',
       };
     default:
       return {
-        color: 'black',
-        backgroundColor: 'white',
+        transformStyle: 'preserve-3d',
+        transform: 'rotateY(0deg)',
       };
   }
 };
 
 const phaseStyle = css`
   min-width: 100px;
-  padding: 2px;
-  border: 1px solid black;
-  border-radius: 0.25em;
+  border: 0px solid black;
+  outline: none;
   cursor: pointer;
   &.completed {
     cursor: default;
+  }
+  height: 20px;
+  .card {
+    border: 1px solid black;
+    border-radius: 0.25em;
+    padding: 2px;
+    width: 100%;
+    height: 100%;
+    position: relative;
+
+    .front,
+    .back {
+      backface-visibility: hidden;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    .front {
+      z-index: 2;
+      transform: rotateY(0deg);
+    }
+    .back {
+      color: white;
+      background-color: green;
+      transform: rotateY(180deg);
+    }
   }
 `;
 
@@ -45,25 +80,33 @@ const PhaseButton: React.FC<
   Merge<ButtonHTMLAttributes<HTMLElement>, { isCompleted: boolean }>
 > = props => {
   const { isCompleted, children, ...nonChildProps } = props;
+  const [currentFlip, setCurrentFlip] = useState(isCompleted);
 
+  // @ts-ignore
   const [propsFlip, setFlip] = useSpring<ISpringType>(() => {
-    const phaseState: PhaseState = isCompleted ? 'complete' : 'default';
-
     return {
-      ...getStateColors(phaseState),
+      ...getStateColors(getFlipState(isCompleted, currentFlip)),
     };
   });
 
   useEffect(() => {
     const phaseState: PhaseState = isCompleted ? 'complete' : 'default';
-    setFlip(getStateColors(phaseState));
-  }, [isCompleted]);
+    setFlip(getStateColors(getFlipState(isCompleted, currentFlip)));
+  }, [isCompleted, currentFlip]);
+
+  const flipButton = (e: MouseEvent<HTMLButtonElement>) => {
+    setCurrentFlip(!currentFlip);
+    console.log('Hit', e.currentTarget);
+  };
 
   const className = `phase ${isCompleted ? 'completed' : ''}`;
   return (
-    <animated.div style={propsFlip} css={phaseStyle} {...nonChildProps}>
-      {children}
-    </animated.div>
+    <button css={phaseStyle} onClick={flipButton} {...nonChildProps}>
+      <animated.div className="card" style={propsFlip}>
+        <div className="back">{children}</div>
+        <div className="front">{children}</div>
+      </animated.div>
+    </button>
   );
 };
 
