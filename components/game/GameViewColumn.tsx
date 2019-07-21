9849bac8b3482e468419bab8bfbf8c94bbbdb16c
Barry <barrymay@Barrys-MacBook-Pro.local>
2019-07-20 21:53:18 -0400

    Simplify GameViewColumn

--- components/game/GameViewColumn.tsx
+++ components/game/GameViewColumn.tsx
@@ -1,33 +1,30 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import React from 'react';
-import { IPlayer } from '../context/PlayersContext';
-import Totaler from './Totaler';
 import { useTournamentCurrentContext } from '../context/TournamentCurrentContext';
 import { useTournamentCurrentRoundContext } from '../context/TournamentCurrentRoundContext';
 import PhaseScorer from './PhaseScorer';
+import Totaler from './Totaler';
 
 const GameViewColumn: React.FC<{ onReady: VoidFunction }> = ({ onReady }) => {
   const { tournament } = useTournamentCurrentContext();
   const { updateMarkedPhase, player } = useTournamentCurrentRoundContext();
 
   return (
-    <React.Fragment key={player.id}>
-      <div className="column">
-        <div className="player-data">{player.name}</div>
-        <div className="player-total">
-          <Totaler playerId={player.id} rounds={tournament.rounds} />
-        </div>
-        <div className="player-data">
-          <PhaseScorer
-            playerId={player.id}
-            rounds={tournament.rounds}
-            onMarkedPhaseUpdate={updateMarkedPhase}
-            onMeasureUpdate={onReady}
-          />
-        </div>
+    <div className="column">
+      <div className="player-data">{player.name}</div>
+      <div className="player-total">
+        <Totaler playerId={player.id} rounds={tournament.rounds} />
       </div>
-    </React.Fragment>
+      <div className="player-data">
+        <PhaseScorer
+          playerId={player.id}
+          rounds={tournament.rounds}
+          onMarkedPhaseUpdate={updateMarkedPhase}
+          onMeasureUpdate={onReady}
+        />
+      </div>
+    </div>
   );
 };
 
