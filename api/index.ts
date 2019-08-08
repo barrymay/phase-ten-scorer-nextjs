d0177042d0a22ea74ed21f297e9f71c44616e84e
barrymay <barrymay@Barrys-MacBook-Pro.local>
2019-08-07 21:59:35 -0400

    Cleanup typescript

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'Hello world!';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
+
+module.exports = server.createHandler({ path: '/api' });
