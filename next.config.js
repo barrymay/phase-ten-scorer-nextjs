8dd8913c5e2d2c3d150ecadcb46a934030938afd
barrymay <barry@foobar.com>
2019-09-09 10:28:08 -0400

    Minor spacing cleanup

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      // For prod this is extraneous, but for dev, this is 
+      // For prod this is extraneous, but for dev, this is
       // added to avoid Invalid Hook Call warning when linking to other libs https://reactjs.org/warnings/invalid-hook-call-warning.html
       react: path.resolve('node_modules/react'),
     };
