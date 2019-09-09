8994de6208f45768d9a1c4e619e16f7223cdd140
barrymay <barry@foobar.com>
2019-09-09 09:36:42 -0400

    Remove one of the ts-ignore's - add next.config.js for easier yarn link handling

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve('node_modules/react'),
+    };
+    return config;
+  },
+};
