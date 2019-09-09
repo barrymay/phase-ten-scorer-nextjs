719d583571301502471e687893a74c9937f9be0d
barrymay <barry@foobar.com>
2019-09-09 09:45:22 -0400

    Added comment to explain react tie-in for next.webpack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
+      // For prod this is extraneous, but for dev, this is 
+      // added to avoid Invalid Hook Call warning when linking to other libs https://reactjs.org/warnings/invalid-hook-call-warning.html
       react: path.resolve('node_modules/react'),
     };
     return config;
