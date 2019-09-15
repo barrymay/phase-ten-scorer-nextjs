e40f7f085dc641577e0e98defc58595a1cc2978c
barrymay <barry@foobar.com>
2019-09-15 14:46:12 -0400

    Add appropriate spec for phase-ten test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,28 +1,20 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
-/// <reference types="Cypress" />
+import '@testing-library/cypress/add-commands';
 
-describe('Batman TV shows', () => {
-  it('has list of shows', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Batman TV Shows')
-    cy.get('li').should('have.length.gt', 5)
-  })
+describe('Index page', () => {
+  /*
+   * Visits the page before each test
+   */
+  beforeEach(() => {
+    cy.log(`Visiting http://localhost:3000`);
+    cy.visit('/');
+  });
 
-  it('goes to the first post', () => {
-    cy.visit('/')
-    cy.get('li')
-      .first()
-      .find('a')
-      .click()
-    cy.url().should('match', /\/p\/\d+$/)
-  })
-
-  it('has About page', () => {
-    cy.visit('/')
-    cy.contains('a', 'About').click()
-    cy.location('pathname').should('equal', '/about')
-    cy.contains('a', 'Home').click()
-    cy.location('pathname').should('equal', '/')
-  })
-})
+  /**
+   * Header section
+   */
+  it('should have a logo', () => {
+    cy.findByText('Player Setup').should('exist');
+    cy.findByText('Create a Tournament').should('exist');
+    cy.findByText('No Tournaments Created').should('exist');
+  });
+});
