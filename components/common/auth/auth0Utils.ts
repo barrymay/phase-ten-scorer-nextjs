b5e8fa6d7e685f7770ed1e3181db34fc5284fc11
barrymay <barry@foobar.com>
2019-09-25 21:29:11 -0400

    Handle enableAuth0 from traveled page

--- components/common/auth/auth0Utils.ts
+++ components/common/auth/auth0Utils.ts
@@ -1,3 +1,8 @@
-export const isAuth0Registered: () => boolean = () => {
+export const isAuth0Registered: (req: any) => boolean = req => {
+  if (!req) {
+    const { enableAuth0 } = (window as any).__NEXT_DATA__.props;
+    return enableAuth0;
+  }
+
   return !!process.env.AUTH0_CLIENT;
 };
