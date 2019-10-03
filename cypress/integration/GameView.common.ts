20b38820deb0851d2835adfce27ccb2e54c143d0
barrymay <barry@foobar.com>
2019-10-02 23:17:42 -0400

    Add wait for now in testGameViewLayout

--- cypress/integration/GameView.common.ts
+++ cypress/integration/GameView.common.ts
@@ -1,4 +1,5 @@
 export const testGameViewLayout = (cy: Cypress.cy, gameName: string) => {
+  cy.wait(4000); // HACK: This should be removed if possible!
   cy.findByText(new RegExp(`^Game: ${gameName}`));
   cy.findByText('Remove Game');
   cy.findByText('Score Round');
