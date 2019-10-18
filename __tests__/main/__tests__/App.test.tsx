4ee2f43a608ee1963d984902a88ddb1b4efb5562
barrymay <barry@foobar.com>
2019-10-18 17:39:44 -0400

    Remove old tests (all newer tests are done on cypress, but we may come back to these)

--- __tests__/main/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from '../../../pages/main/App';
-import { render } from 'react-testing-library';
-
-it('renders without crashing', () => {
-  const { unmount } = render(<App />);
-  unmount();
-});
