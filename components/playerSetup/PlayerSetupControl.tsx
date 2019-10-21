cf9185028bf0ae0f74f7095aa2159b80470c4240
barrymay <barry@foobar.com>
2019-10-21 17:35:39 -0400

    Confirm when deleted

--- components/playerSetup/PlayerSetupControl.tsx
+++ components/playerSetup/PlayerSetupControl.tsx
@@ -8,6 +8,7 @@ import { CardContainer } from '../common/styles/basic';
 import { usePlayersDispatch, usePlayersState } from '../context/PlayersContext';
 import PlayerList from './PlayerList';
 import { useAppTheme } from '../theming/AppThemeProvider';
+import ConfirmModal from '../common/ConfirmModal';
 
 const PlayerNameInput = styled.input`
   max-width: 200px;
@@ -21,11 +22,19 @@ const PlayerEntry = styled.div({
 
 export const PlayerSetupControl: React.FC = () => {
   const theme = useAppTheme();
+  const [showModal, setShowModal] = useState<'removeAll' | undefined>(
+    undefined,
+  );
   const dispatchPlayers = usePlayersDispatch();
   const players = usePlayersState();
   const [name, setName] = useState('');
   const [triggerFocus, setTriggerFocus] = useState(false);
   const textRef: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+
+  const hideModal = () => {
+    setShowModal(undefined);
+  };
+
   const addPlayer = (name: string) => {
     dispatchPlayers({ type: 'ADD', playerName: name.trim() });
     setName('');
@@ -37,8 +46,13 @@ export const PlayerSetupControl: React.FC = () => {
   };
 
   const clearAll = () => {
+    setShowModal('removeAll');
+  };
+
+  const confirmClearAll = () => {
     dispatchPlayers({ type: 'SET', players: [] });
     setName('');
+    setShowModal(undefined);
   };
 
   const keyHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -77,6 +91,14 @@ export const PlayerSetupControl: React.FC = () => {
         }
       `}
     >
+      <ConfirmModal
+        isShown={showModal === 'removeAll'}
+        onCloseModal={hideModal}
+        onConfirmModal={confirmClearAll}
+      >
+        Are you sure you want to clear all players?
+      </ConfirmModal>
+
       <div className="header">Player Setup</div>
       <CardContainer theme={theme}>
         <div
@@ -130,7 +152,18 @@ export const PlayerSetupControl: React.FC = () => {
           </div>
           <div>
             <label data-testid="player-counter">Players:</label>
-            <PlayerList players={players} />
+            {players.length ? (
+              <PlayerList players={players} />
+            ) : (
+              <div
+                css={css`
+                  display: flex;
+                  justify-content: center;
+                `}
+              >
+                No Players Defined
+              </div>
+            )}
           </div>
           <div
             css={css`
