91d34770b5a6538428994acc9f389decf1a61847
barrymay <barry@foobar.com>
2021-04-24 21:15:05 -0400

    Test if longer wait helps

--- cypress/integration/GameView.common.ts
+++ cypress/integration/GameView.common.ts
@@ -1,5 +1,5 @@
-export const testGameViewLayout = (cy: Cypress.cy, gameName: string) => {
-  cy.wait(4000); // HACK: This should be removed if possible!
+export const testGameViewLayout = (cy: Cypress.cy, gameName: string): void => {
+  cy.wait(10000); // HACK: This should be removed if possible!
   cy.findByText(new RegExp(`^Game: ${gameName}`));
   cy.findByText('Remove Game');
   cy.findByText('Score Round');
